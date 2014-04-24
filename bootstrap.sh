e977379656afc165f0b7957e002b5d69ec9e28ce
Kristoffer <hello@kristoffer.is>
2014-04-24 21:53:45 +0200

    Ask for and keep sudo while the script is running

--- bootstrap.sh
+++ bootstrap.sh
@@ -96,5 +96,13 @@ install_dotfiles () {
   done
 }
 
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.osx` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+install_dotfiles
+
 # find the installers and run them iteratively
 find . -name install.sh | while read installer ; do sh -c "${installer}" ; done
