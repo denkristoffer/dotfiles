2f3a8614bb76f08acac12abbeb4346d8017a5c7b
Kristoffer <hello@kristoffer.is>
2014-04-24 22:54:34 +0200

    Move sudo back to osx install file

--- osx/install.sh
+++ osx/install.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
+#
+# My choice of OS X settings based on http://mths.be/osx
 
-# ~/.osx — http://mths.be/osx
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.osx` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 ###############################################################################
 # General UI/UX                                                               #
@@ -142,7 +148,6 @@ defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
 defaults write NSGlobalDomain com.apple.springing.delay -float 0
 
 # Enable snap-to-grid for icons on the desktop and in other icon views
-/usr/libexec/PlistBuddy -c "Set :FK_StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
 /usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
 
 # Use column view in all Finder windows by default
