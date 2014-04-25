f3c2d4fe9dfc752c29dceb945e8723e8eef4f25b
Kristoffer <hello@kristoffer.is>
2014-04-25 16:43:54 +0200

    Try sed -n

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -6,9 +6,10 @@
 if test ! $(which brew)
 then
   echo "  Installing Homebrew for you."
-  ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)" 2>&1 | sed -e "s/^/  /"
+  ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)" 2>&1 | sed -ne "s/^/  /"
 
   exit 0
 fi
 
+echo "  Homebrew is already installed. Updating it: "
 brew update | sed "s/^/  /"
