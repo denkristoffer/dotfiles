d39ef68a21b6f2779cb54e9ceb77888afad6c6df
Kristoffer <hello@kristoffer.is>
2014-04-25 12:39:31 +0200

    No need to update Homebrew right after installation

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 #
-# Install Homebrew and a few select packages
+# Install Homebrew
 
 # Check for Homebrew
 if test ! $(which brew)
 then
   echo "  Installing Homebrew for you."
-  ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+  ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)" | sed "s/^/  /"
+
+  exit 0
 fi
 
 brew update | sed "s/^/  /"
-
-exit 0
