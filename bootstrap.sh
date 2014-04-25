5bad0e4d86df427747394e73c22f475641fe2f99
Kristoffer <hello@kristoffer.is>
2014-04-25 15:15:43 +0200

    Use %s

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,25 +1,25 @@
 #!/usr/bin/env bash
 
-DOTFILES_DIR="`pwd`"
+DOTFILES_DIR="$(pwd)"
 
 set -e
 
 echo ''
 
 info () {
-  printf "  [ \033[00;34m..\033[0m ] $1"
+  printf "  [ \033[00;34m..\033[0m ] %s", "$1"
 }
 
 user () {
-  printf "\r  [ \033[0;33m?\033[0m ] $1 "
+  printf "\r  [ \033[0;33m?\033[0m ] %s ", "$1"
 }
 
 success () {
-  printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
+  printf "\r\033[2K  [ \033[00;32mOK\033[0m ] %s\n", "$1"
 }
 
 fail () {
-  printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
+  printf "\r\033[2K  [\033[0;31mFAIL\033[0m] %s\n", "$1"
   echo ''
   exit
 }
