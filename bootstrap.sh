61874d39ce430d26ae9b8acc50a012efed18889d
Kristoffer <hello@kristoffer.is>
2014-04-25 16:02:00 +0200

    Try it without sudo

--- bootstrap.sh
+++ bootstrap.sh
@@ -96,10 +96,10 @@ install_dotfiles () {
 }
 
 # Ask for the administrator password upfront
-sudo -v
+#sudo -v
 
 # Keep-alive: update existing `sudo` time stamp until the script has finished
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+#while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 echo ''
 
