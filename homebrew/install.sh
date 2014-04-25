3444875356a2c10bd604c03cff9b4b1a5fafdb2f
Kristoffer <hello@kristoffer.is>
2014-04-25 16:49:18 +0200

    Don't bother with updating Homebrew
    It’s unnecessary right after installing, and at the moment I don’t see
    myself running this script regularly to update instead of just using
    `brew update`. Might change in the future.

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -7,9 +7,4 @@ if test ! $(which brew)
 then
   echo "  Installing Homebrew for you."
   ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)" 2>&1 | sed -ne "s/^/  /"
-
-  exit 0
 fi
-
-echo "  Homebrew is already installed. Updating it: "
-brew update | sed "s/^/  /"
