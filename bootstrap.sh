ae26a463f3216b321d40ae57e07649af878754a9
Kristoffer <hello@kristoffer.is>
2014-04-25 15:21:59 +0200

    The fail method isn't used, comment it out

--- bootstrap.sh
+++ bootstrap.sh
@@ -18,11 +18,12 @@ success () {
   printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
 }
 
-fail () {
-  printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
-  echo ''
-  exit
-}
+# This isn't currently in use
+# fail () {
+#   printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
+#   echo ''
+#   exit
+# }
 
 link_files () {
   ln -s $1 $2
