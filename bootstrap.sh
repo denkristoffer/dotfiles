afafc7b14181d0b1ba5d2b53ebe538e574ad222a
Kristoffer <hello@kristoffer.is>
2014-04-25 15:16:52 +0200

    Comma showed up, revert use of %s

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,19 +7,19 @@ set -e
 echo ''
 
 info () {
-  printf "  [ \033[00;34m..\033[0m ] %s", "$1"
+  printf "  [ \033[00;34m..\033[0m ] $1"
 }
 
 user () {
-  printf "\r  [ \033[0;33m?\033[0m ] %s ", "$1"
+  printf "\r  [ \033[0;33m?\033[0m ] $1 "
 }
 
 success () {
-  printf "\r\033[2K  [ \033[00;32mOK\033[0m ] %s\n", "$1"
+  printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
 }
 
 fail () {
-  printf "\r\033[2K  [\033[0;31mFAIL\033[0m] %s\n", "$1"
+  printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
   echo ''
   exit
 }
