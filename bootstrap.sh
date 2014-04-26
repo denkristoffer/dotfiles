8de3f9ce925d87796cba63468b4d37376d3f523f
Kristoffer <hello@kristoffer.is>
2014-04-26 21:02:15 +0200

    Keep sudo

--- bootstrap.sh
+++ bootstrap.sh
@@ -99,7 +99,7 @@ install_dotfiles () {
 sudo -v
 
 # Keep-alive: update existing `sudo` time stamp until the script has finished
-#while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 echo ''
 
