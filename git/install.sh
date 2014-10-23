b16ca044cea941038f6198793afafed5804833bb
Kristoffer <hello@kristoffer.is>
2014-10-23 21:31:41 +0200

    Better if statement

--- git/install.sh
+++ git/install.sh
@@ -2,7 +2,7 @@
 #
 # Install Git
 
-if [ "$(which git)" != '/usr/local/bin/git' ]; then
+if [[ "$(which git)" != '/usr/local/bin/git' ]]; then
   echo "  Installing Git for you."
   brew install git > /tmp/git-install.log
 fi
