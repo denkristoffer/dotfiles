ce63c51fb3e0cc32cb4769d69429a1a6b0905c74
Kristoffer <hello@kristoffer.is>
2016-10-03 20:49:38 +0200

    Add back Homebrew Cask

--- homebrew-cask/install.sh
+++ homebrew-cask/install.sh
@@ -2,30 +2,39 @@
 #
 # Install Homebrew Cask
 
-# DISABLED until Homebrew Cask's upcoming changes to move from linking to
-# copying/moving are completed
-
 # Check for Homebrew
 printf "\n  Installing Homebrew Cask apps for you.\n"
 
 # Install Cask apps
 brew cask install \
 alfred \
-backblaze \
-chitchat \
-crashplan \
+appcleaner \
+bartender \
+beardedspice \
+caprine \
+controlplane \
 daisydisk \
 dropbox \
 flux \
+github-desktop \
+go2shell-chrome \
 google-chrome \
 hazel \
+imageoptim \
 iterm2 \
+kodi \
 little-snitch \
+musicbrainz-picard \
 openemu \
+openpht \
+paw \
 postgres \
 postico \
+scroll-reverser \
 seil \
+selfcontrol \
 sublime-text \
+torbrowser \
 transmission \
 viscosity \
 xld
