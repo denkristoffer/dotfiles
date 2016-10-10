a8da5a1154986e43fe0ca7f89478678375f00156
Kristoffer <hello@kristoffer.is>
2016-10-10 15:24:02 +0200

    Log Cask output to file

--- homebrew-cask/install.sh
+++ homebrew-cask/install.sh
@@ -7,34 +7,33 @@ printf "\n  Installing Homebrew Cask apps for you.\n"
 
 # Install Cask apps
 brew cask install \
-alfred \
-appcleaner \
-bartender \
-beardedspice \
-caprine \
-controlplane \
-daisydisk \
-dropbox \
-flux \
-github-desktop \
-go2shell-chrome \
-google-chrome \
-hazel \
-imageoptim \
-iterm2 \
-kodi \
-little-snitch \
-musicbrainz-picard \
-openemu \
-openpht \
-paw \
-postgres \
-postico \
-scroll-reverser \
-seil \
-selfcontrol \
-sublime-text \
-torbrowser \
-transmission \
-viscosity \
-xld
+    alfred \
+    appcleaner \
+    atom \
+    bartender \
+    beardedspice \
+    caprine \
+    controlplane \
+    daisydisk \
+    dropbox \
+    flux \
+    github-desktop \
+    go2shell \
+    google-chrome \
+    hazel \
+    imageoptim \
+    iterm2 \
+    kodi \
+    little-snitch \
+    musicbrainz-picard \
+    openemu \
+    openpht \
+    paw \
+    postgres \
+    postico \
+    scroll-reverser \
+    selfcontrol \
+    torbrowser \
+    transmission \
+    viscosity \
+    xld > /tmp/cask-install.log
