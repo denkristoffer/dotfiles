8be2053a661c5b67203aacd8df44a08c1c376131
Kristoffer <hello@kristoffer.is>
2016-10-11 17:00:58 +0200

    Add nvm to node installation

--- node/install.sh
+++ node/install.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 #
-# Install Node
+# Install nvm and Node
 
 # Check for Node
+if test ! $(which nvm)
+then
+  printf "\n  Installing nvm for you.\n"
+  brew install nvm > /tmp/nvm-install.log
+fi
+
 if test ! $(which node)
 then
   printf "\n  Installing Node for you.\n"
-  brew install node > /tmp/node-install.log
+  nvm install node > /tmp/node-install.log
 fi
