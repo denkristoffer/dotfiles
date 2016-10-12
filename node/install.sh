a6aead65d52b2fdc7d0735e85a923585ab60147a
Kristoffer <hello@kristoffer.is>
2016-10-12 21:17:43 +0200

    Try to load nvm for install

--- node/install.sh
+++ node/install.sh
@@ -5,5 +5,7 @@
 if test ! $(which node)
 then
   printf "\n› Installing Node\n"
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
   nvm install node > /tmp/node-install.log
 fi
