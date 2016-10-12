d71dd30eacdc425307670fd62d86b506e83ce6e2
Kristoffer <hello@kristoffer.is>
2016-10-12 20:38:10 +0200

    Remove superfluous installs
    I forgot

--- node/install.sh
+++ node/install.sh
@@ -1,16 +1,9 @@
 #!/bin/sh
 #
-# Install nvm and Node
-
-# Check for Node
-if test ! $(which nvm)
-then
-  printf "\n  Installing nvm for you.\n"
-  brew install nvm > /tmp/nvm-install.log
-fi
+# Install Node with nvm
 
 if test ! $(which node)
 then
-  printf "\n  Installing Node for you.\n"
+  printf "\n› Installing Node\n"
   nvm install node > /tmp/node-install.log
 fi
