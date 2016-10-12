4aae2fdb148d4164926ec6ebf2c0f545d616c279
Kristoffer <hello@kristoffer.is>
2016-10-12 22:48:53 +0200

    Point to specific Ruby binary
    Like the brew.sh installation command

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -5,10 +5,10 @@
 if test ! $(which brew)
 then
   printf "\n› Installing Homebrew\n"
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" 2>&1 </dev/null | sed "s/^/  /"
 
   printf "\n› brew update\n"
   brew update
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" 2>&1 </dev/null
 fi
 
 exit 0
