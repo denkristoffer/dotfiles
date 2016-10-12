c0cccf9b0022a2b8e8ddbb412513b570dd9d7094
Kristoffer <hello@kristoffer.is>
2016-10-12 22:47:21 +0200

    Update for Brewfile installation

--- puma-dev/install.sh
+++ puma-dev/install.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
-#
-# Install pow
 
-if test ! $(which puma-dev)
-then
-    printf "\n  Installing puma-dev for you.\n"
-    brew install puma/puma/puma-dev > /tmp/puma-dev-install.log
-    sudo puma-dev -setup
+printf "\n› Setting up puma-dev\n"
+sudo puma-dev -setup
 
-    # Setup port 80 and 443 forwarding
-    puma-dev -install
-fi
+# Setup port 80 and 443 forwarding
+puma-dev -install
