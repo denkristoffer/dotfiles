b2c73dc1746378415bf18dc2181debf5952ae8c8
Kristoffer <hello@kristoffer.is>
2016-10-12 22:49:53 +0200

    Copy files instead of symlinking
    Mackup should take care of backing everything up, so it’d be easier to
    have all the zsh files in one place

--- zsh/install.sh
+++ zsh/install.sh
@@ -10,15 +10,15 @@ sudo echo "/usr/local/bin/zsh\n" >> /etc/shells
 # Install oh-my-zsh
 curl -L http://install.ohmyz.sh | sh > /tmp/oh-my-zsh-install.log
 
-# Symlink kristoffer.zsh-theme to $HOME/.oh-my-zsh/themes/
-ln -s $(pwd)/kristoffer.zsh-theme $HOME/.oh-my-zsh/themes/kristoffer.zsh-theme
+# Copy kristoffer.zsh-theme to $HOME/.oh-my-zsh/themes/
+cp -s $(pwd)/kristoffer.zsh-theme $HOME/.oh-my-zsh/themes/kristoffer.zsh-theme
 
-# Symlink all zsh files to $HOME/.oh-my-zsh/custom
+# Copy all zsh files to $HOME/.oh-my-zsh/custom
 typeset -U config_files
 config_files=($(pwd)/../**/*.zsh)
 
 for file in ${config_files}
 do
   filename=$(basename "$file")
-  ln -s $file $HOME/.oh-my-zsh/custom/$filename
+  cp $file $HOME/.oh-my-zsh/custom/$filename
 done
