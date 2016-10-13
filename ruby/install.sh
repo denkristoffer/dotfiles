af805fbbee7600308f2a3b8c8c91c40612550343
Kristoffer <hello@kristoffer.is>
2016-10-13 13:10:06 +0200

    Add $PATH in attempt to fix gem installation

--- ruby/install.sh
+++ ruby/install.sh
@@ -3,5 +3,9 @@
 # Install latest stable version of Ruby
 printf "\n› Installing latest Ruby stable with bundler\n"
 . /usr/local/opt/chruby/share/chruby/chruby.sh
+
+export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
+
 ruby-install --latest --cleanup --no-reinstall ruby &> /tmp/ruby-install.log && rm -rf $HOME/src
-# gem install bundler
+
+gem install bundler
