f5c476bf49d2820bd947f59f8ac2c32c73793e0a
Kristoffer <hello@kristoffer.is>
2016-10-13 08:57:58 +0200

    Source chruby before installing ruby

--- ruby/install.sh
+++ ruby/install.sh
@@ -2,5 +2,6 @@
 #
 # Install latest stable version of Ruby
 printf "\n› Installing latest Ruby stable with bundler\n"
+source "$(brew --prefix chruby)/chruby.sh"
 ruby-install --latest --cleanup --no-reinstall ruby &> /tmp/ruby-install.log && rm -rf $HOME/src
-sh -c "gem install bundler"
+gem install bundler
