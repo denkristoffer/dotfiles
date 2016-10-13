36b8e885d1261c8c52edb18c1aef6e090d502f78
Kristoffer <hello@kristoffer.is>
2016-10-13 08:16:18 +0200

    Fix command order

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -6,9 +6,10 @@ if test ! $(which brew)
 then
   printf "\n› Installing Homebrew\n"
 
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" 2>&1 </dev/null
+
   printf "\n› brew update\n"
   brew update
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" 2>&1 </dev/null
 fi
 
 exit 0
