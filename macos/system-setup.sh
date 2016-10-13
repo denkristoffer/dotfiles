c5d894d326dcde9b1f68d17cf54fccb63436c126
Kristoffer <hello@kristoffer.is>
2016-10-13 15:23:20 +0200

    Add new settings

--- macos/system-setup.sh
+++ macos/system-setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# My choice of OS X settings based on http://mths.be/osx
+# My choice of OS X settings based on https://github.com/herrbischoff/awesome-osx-command-line and http://mths.be/macos
 
 # Ask for the administrator password upfront
 sudo -v
@@ -19,21 +19,50 @@ echo "0x08000100:0" > ~/.CFUserTextEncoding
 # Restart automatically if the computer freezes
 sudo systemsetup -setrestartfreeze on
 
-# Disable auto-correct
-defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+# Enable Safari Develop Menu and Web Inspector
+defaults write com.apple.Safari IncludeInternalDebugMenu -bool true && \
+defaults write com.apple.Safari IncludeDevelopMenu -bool true && \
+defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true && \
+defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true && \
+defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
 
-# Show the ~/Library folder
+# Prevent Time Machine from Prompting to Use New Hard Drives as Backup Volume
+defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
+
+# Finder
+## Show All File Extensions
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+## Show the ~/Library folder
 chflags nohidden ~/Library
 
-# Expand save panel by default
-defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+## Show "Quit Finder" Menu Item
+defaults write com.apple.finder QuitMenuItem -bool true
 
-# Expand print panel by default
-defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
+## Expand save panel
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true && \
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
+
+## Expand print panel
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true && \
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
+
+# Show Status Bar
+defaults write com.apple.finder ShowStatusBar -bool true
+
+# Set Default Finder Location to Home Folder
+defaults write com.apple.finder NewWindowTarget -string "PfLo" && \
+defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}"
+
+# Disable auto-correct
+defaults write -g NSAutomaticSpellingCorrectionEnabled -bool false
+
+# Enable firewall
+sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setglobalstate on
 
 # Check for software updates daily, not just once per week
 defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
 
-for app in "cfprefsd" "SystemUIServer"; do
+for app in "Finder" "SystemUIServer"; do
     killall "${app}" &> /dev/null
 done
