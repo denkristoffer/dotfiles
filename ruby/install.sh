6bfb8240d544cc3639998b9d6595cfd25cbd4948
Kristoffer <hello@kristoffer.is>
2016-10-13 08:52:24 +0200

    Try installing bundler gem from new shell

--- ruby/install.sh
+++ ruby/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
 # Install latest stable version of Ruby
-printf "\n› Installing latest Ruby stable\n"
+printf "\n› Installing latest Ruby stable with bundler\n"
 ruby-install --latest --cleanup --no-reinstall ruby &> /tmp/ruby-install.log && rm -rf $HOME/src
-gem install bundler
+sh -c "gem install bundler"
