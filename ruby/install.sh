7ca888145a9c1069da47363cb0bb7d6d0a36a9c9
Kristoffer <hello@kristoffer.is>
2016-10-13 09:06:30 +0200

    Source explicit chruby path

--- ruby/install.sh
+++ ruby/install.sh
@@ -2,6 +2,6 @@
 #
 # Install latest stable version of Ruby
 printf "\n› Installing latest Ruby stable with bundler\n"
-source "$(brew --prefix chruby)/chruby.sh"
+. /usr/local/opt/chruby/share/chruby/chruby.sh
 ruby-install --latest --cleanup --no-reinstall ruby &> /tmp/ruby-install.log && rm -rf $HOME/src
 gem install bundler
