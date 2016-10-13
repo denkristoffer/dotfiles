c0becfcb015d0ee3a4bbab1518627c97fd139228
Kristoffer <hello@kristoffer.is>
2016-10-13 08:20:08 +0200

    Temp fix: Create LaunchAgents dir

--- puma-dev/install.sh
+++ puma-dev/install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 printf "\n› Setting up puma-dev\n"
+mkdir $HOME/Library/LaunchAgents # Shouldn't need this much longer: https://github.com/puma/puma-dev/pull/77
 sudo puma-dev -setup
 
 # Setup port 80 and 443 forwarding
