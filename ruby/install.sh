9becbc1993786a3214c8e394473a07df62733bf5
Kristoffer <hello@kristoffer.is>
2016-10-13 09:10:08 +0200

    Can't get gem install working

--- ruby/install.sh
+++ ruby/install.sh
@@ -4,4 +4,4 @@
 printf "\n› Installing latest Ruby stable with bundler\n"
 . /usr/local/opt/chruby/share/chruby/chruby.sh
 ruby-install --latest --cleanup --no-reinstall ruby &> /tmp/ruby-install.log && rm -rf $HOME/src
-gem install bundler
+# gem install bundler
