acba56857d32879e973597fe2e5954abe43b5db7
Kristoffer <hello@kristoffer.is>
2016-10-13 08:38:15 +0200

    Install bundler

--- ruby/install.sh
+++ ruby/install.sh
@@ -3,3 +3,4 @@
 # Install latest stable version of Ruby
 printf "\n› Installing latest Ruby stable\n"
 ruby-install --latest --cleanup --no-reinstall ruby &> /tmp/ruby-install.log && rm -rf $HOME/src
+gem install bundler
