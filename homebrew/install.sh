7e7b940caa85edb21086f5d5f1bf812a5e23511e
Kristoffer <hello@kristoffer.is>
2016-10-13 08:41:16 +0200

    Output Homebrew install to a log

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -6,7 +6,7 @@ if test ! $(which brew)
 then
   printf "\n› Installing Homebrew\n"
 
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" 2>&1 </dev/null
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" &> /tmp/homebrew-install.log
 
   printf "\n› brew update\n"
   brew update
