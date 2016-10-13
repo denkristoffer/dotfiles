d0ad0f7f29f2e4f3326ff42c64fe6cebfd69ed46
Kristoffer <hello@kristoffer.is>
2016-10-13 15:08:15 +0200

    Move asking for sudo to system setup

--- bootstrap.sh
+++ bootstrap.sh
@@ -110,12 +110,6 @@ install_dotfiles () {
   done
 }
 
-# Ask for the administrator password upfront
-sudo -v
-
-# Keep-alive: update existing `sudo` time stamp until the script has finished
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
-
 install_dotfiles
 
 # Install Homebrew first, as it's needed for almost everything else
--- macos/system-setup.sh
+++ macos/system-setup.sh
@@ -2,6 +2,12 @@
 #
 # My choice of OS X settings based on http://mths.be/osx
 
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until the script has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 # Close any open System Preferences panes, to prevent them from overriding
 # settings we’re about to change
 osascript -e 'tell application "System Preferences" to quit'
