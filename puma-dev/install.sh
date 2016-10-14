ee76a789afb4032041735bf31c0cdf769afb2305
Kristoffer <hello@kristoffer.is>
2016-10-14 14:21:37 +0200

    Chown LaunchAgents dir to user

--- puma-dev/install.sh
+++ puma-dev/install.sh
@@ -2,7 +2,9 @@
 
 printf "\n› Setting up puma-dev\n"
 sudo -K
-mkdir $HOME/Library/LaunchAgents # Shouldn't need this much longer: https://github.com/puma/puma-dev/pull/77
+echo $(whoami)
+mkdir $HOME/Library/LaunchAgents && chown kristoffer $HOME/Library/LaunchAgents # Shouldn't need this much longer: https://github.com/puma/puma-dev/pull/77
+
 sudo puma-dev -setup
 
 # Setup port 80 and 443 forwarding
