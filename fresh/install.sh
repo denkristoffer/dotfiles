9d110492e80a53595a22c250ee095292fd468c86
Kristoffer <hello@kristoffer.is>
2020-04-03 22:29:42 +0200

    Update fresh installation method

--- fresh/install.sh
+++ fresh/install.sh
@@ -4,9 +4,7 @@ if test ! $(which fresh)
 then
   printf "\n› Installing fresh\n"
 
-  git clone https://github.com/freshshell/fresh ~/.fresh/source/freshshell/fresh
-  echo "fresh freshshell/fresh bin/fresh --bin" >> ~/.freshrc
-  ~/.fresh/source/freshshell/fresh/bin/fresh
+  /bin/bash -c "`curl -sL https://get.freshshell.com`" </dev/null &> /tmp/fresh-install.log
 
   # Add `source ~/.fresh/build/shell.sh` to your shell config.
   source ~/.fresh/build/shell.sh
