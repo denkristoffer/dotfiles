08d69a8e815f3ef03c1ed20f1ad7c27730a21345
Kristoffer Sachse <kristoffer@Kristoffers-Pro.domain_not_set.invalid>
2020-04-06 12:34:40 +0200

    Initial exports

--- shell/exports.sh
+++ shell/exports.sh
@@ -1,1 +1,11 @@
+path_add() {
+  if [ -d "$1" ] && [[ ":$PATH:" != *":$1:"* ]]; then
+    export PATH="${PATH:+"$PATH:"}$1"
+  fi
+}
+
 export DOTFILES=~/.dotfiles
+# export PATH=$HOME/bin:/usr/local/bin:$PATH
+# export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
+export ZSH=$HOME/.oh-my-zsh
+
