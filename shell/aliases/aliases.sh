08b51401dc519d40a56d57a9d61cff92a4c6f64d
denkristoffer <denkristoffer@users.noreply.github.com>
2020-05-07 00:33:30 +0200

    Add reset! alias

--- shell/aliases/aliases.sh
+++ shell/aliases/aliases.sh
@@ -1,4 +1,5 @@
 alias brwe='brew'
 alias reload!='source $HOME/.zshrc'
+alias reset!='clear && printf "\e[3J"' # https://askubuntu.com/a/473770
 
 hash -d icloud="$HOME/Library/Mobile Documents/com~apple~CloudDocs"
