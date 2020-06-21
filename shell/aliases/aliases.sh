e9c66f7a51094afc8d5fb7c6ab363b2180f8b19b
denkristoffer <denkristoffer@users.noreply.github.com>
2020-06-21 22:18:16 +0200

    feat: add hashes for books and podcasts

--- shell/aliases/aliases.sh
+++ shell/aliases/aliases.sh
@@ -2,4 +2,6 @@ alias brwe='brew'
 alias reload!='source $HOME/.zshrc'
 alias reset!='clear && printf "\e[3J"' # https://askubuntu.com/a/473770
 
+hash -d books="$HOME/Library/Containers/com.apple.BKAgentService/Data/Documents/iBooks"
 hash -d icloud="$HOME/Library/Mobile Documents/com~apple~CloudDocs"
+hash -d podcasts="$HOME/Library/Group Containers/243LU875E5.groups.com.apple.podcasts"
