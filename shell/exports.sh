31a6b74db54077f11b8a9569e5f505465ece922b
denkristoffer <denkristoffer@users.noreply.github.com>
2021-03-06 09:43:47 +0100

    Disable Next.js telemetry globally

--- shell/exports.sh
+++ shell/exports.sh
@@ -11,6 +11,9 @@ export LANG=da_DK.UTF-8
 # 3 days, 60 * 60 * 24 * 3
 export HOMEBREW_AUTO_UPDATE_SECS=259200
 
+# Disable Next.js telemetry globally
+export NEXT_TELEMETRY_DISABLED=1
+
 path_add "/usr/local/sbin"
 path_add "/usr/local/sbin"
 path_add "$HOME/.fastlane/bin"
