1ce2399b8c98f5bd4d145131efe62363ac3be7ac
Kristoffer <denkristoffer@users.noreply.github.com>
2022-02-05 16:27:24 +0100

    🔨 cleanup

--- config/hammerspoon/bluetooth.lua
+++ config/hammerspoon/bluetooth.lua
@@ -1,7 +1,4 @@
--- local bluetooth = require "hs._asm.undocumented.bluetooth"
-
 hs.hotkey.bind(hyper, "b", function()
-  -- bluetooth.power(true)
   local _, output = hs.osascript.applescriptFromFile("toggle-headphones.applescript")
   hs.alert.show(output)
 end)
