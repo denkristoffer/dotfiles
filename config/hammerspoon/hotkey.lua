1e4e698a8c79333f9bca8f56e24e679af16050b4
Kristoffer <denkristoffer@users.noreply.github.com>
2022-02-05 23:11:09 +0100

    🐛 try backslash keybind

--- config/hammerspoon/hotkey.lua
+++ config/hammerspoon/hotkey.lua
@@ -1,11 +1,11 @@
--- Al
+-- Alfred
 hyper:bind({}, 'space', function()
   hs.eventtap.keyStroke({'cmd','alt','shift','ctrl'}, 'space')
   hyper.triggered = true
 end)
 
--- iTer
-hyper:bind({}, '$', function()
+-- iTerm
+hyper:bind({}, '\\', function()
   hs.eventtap.keyStroke({'cmd','alt','shift','ctrl'}, '$')
   hyper.triggered = true
 end)
