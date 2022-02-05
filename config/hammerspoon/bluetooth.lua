545a1095eb70e6ee6a6b828a4da49400f30ae4ff
Kristoffer <denkristoffer@users.noreply.github.com>
2022-02-05 14:08:24 +0100

    🔨 move things around

--- config/hammerspoon/bluetooth.lua
+++ config/hammerspoon/bluetooth.lua
@@ -1,7 +1,7 @@
-local bluetooth = require "hs._asm.undocumented.bluetooth"
+-- local bluetooth = require "hs._asm.undocumented.bluetooth"
 
 hs.hotkey.bind(hyper, "b", function()
-  bluetooth.power(true)
-  local _, output = hs.osascript.applescriptFromFile("~/.hammerspoon/connect-headphones.applescript")
+  -- bluetooth.power(true)
+  local _, output = hs.osascript.applescriptFromFile("headphones.applescript")
   hs.alert.show(output)
 end)
