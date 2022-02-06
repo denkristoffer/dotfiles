e95d2f78e05db8def43f2d595d02e1fa575d33f5
Kristoffer <denkristoffer@users.noreply.github.com>
2022-02-06 22:45:47 +0100

    🐛 messed up key stroke

--- config/hammerspoon/hotkey.lua
+++ config/hammerspoon/hotkey.lua
@@ -6,12 +6,12 @@ end)
 
 -- Toggle Dock
 hyper:bind({}, 'd', function()
-  hs.eventtap.keyStroke({'cmd','shift'}, 'd')
+  hs.eventtap.keyStroke({'cmd','alt'}, 'd')
   hyper.triggered = true
 end)
 
 -- iTerm
-hyper:bind({}, '$', function()
+hyper:bind({}, 93, function()
   hs.eventtap.keyStroke({'cmd','alt','shift','ctrl'}, '$')
   hyper.triggered = true
 end)
