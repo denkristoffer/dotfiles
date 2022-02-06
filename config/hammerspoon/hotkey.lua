719d74ccb96b997184e1843fce53e2a8075770d1
Kristoffer <denkristoffer@users.noreply.github.com>
2022-02-06 22:38:05 +0100

    ✨ toggle Dock hiding

--- config/hammerspoon/hotkey.lua
+++ config/hammerspoon/hotkey.lua
@@ -4,8 +4,14 @@ hyper:bind({}, 'space', function()
   hyper.triggered = true
 end)
 
+-- Toggle Dock
+hyper:bind({}, 'd', function()
+  hs.eventtap.keyStroke({'cmd','shift'}, 'd')
+  hyper.triggered = true
+end)
+
 -- iTerm
-hyper:bind({}, 93, function()
+hyper:bind({}, '$', function()
   hs.eventtap.keyStroke({'cmd','alt','shift','ctrl'}, '$')
   hyper.triggered = true
 end)
