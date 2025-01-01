21679c85967b20c9ede5bf287b7a0e06964b7c68
denkristoffer <denkristoffer@users.noreply.github.com>
2025-01-01 19:37:21 +0100

    Revert "🐛 use homebrew prefix"
    This reverts commit c00d971be1c30e2b9cf460863cec9f460e4f1dde.

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -7,7 +7,7 @@ then
   printf "\n› Installing Homebrew\n"
 
   NOINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" </dev/null &> /tmp/homebrew-install.log
-  eval "$($HOMEBREW_PREFIX/bin/brew shellenv)"
+  eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
 brew analytics off
